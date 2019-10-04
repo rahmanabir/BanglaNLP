eb2a024eaca86de5826300cf66b30932ec4507d0
Jawad Aziz Khan <jawad.aziz.khan.jak@gmail.com>
2019-10-04 14:23:52 +0600

    loader fix  and csv fix kinda

--- data_dataloader.py
+++ data_dataloader.py
@@ -20,25 +20,30 @@ warnings.filterwarnings('ignore')
 
 # ------------------------------------------------
 
+
 def displayimage(imgarr, r=1, c=3):
-    plt.figure(figsize=(18,32))
+    plt.figure(figsize=(18, 32))
     count = 1
-    for i in range(1,r+1):
-        for j in range(1,c+1):
-            plt.subplot(r,c,count)
+    for i in range(1, r+1):
+        for j in range(1, c+1):
+            plt.subplot(r, c, count)
             plt.imshow(imgarr[count-1])
             count += 1
     plt.show()
+
 
 def torchimshow(img):
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
 
+
 def torchdatagrid(images):
-    plt.figure(figsize=(16,8))
+    plt.figure(figsize=(16, 8))
     torchimshow(torchvision.utils.make_grid(images))
 
 # ------------------------------------------------------------------
+
+
 def saveimagesasnpy(dir='data/spectographs/', csvf='dekhabet_dataLabelsRanged.csv', name='2k2sec'):
     labels = []
     fnames = []
@@ -50,14 +55,14 @@ def saveimagesasnpy(dir='data/spectographs/', csvf='dekhabet_dataLabelsRanged.cs
             text = row[4]
             text = text.strip("'-!$[]")
             text = text.split(',')
-            i=0
-            for t in range(0,44):
+            i = 0
+            for t in range(0, 43):
                 if 'Token' in row[4]:
                     pass
                 else:
                     try:
                         ctext.append(int(text[t])+1)
-                        i+=1
+                        i += 1
                         # print(text[t])
                     except IndexError:
                         ctext.append(0)
@@ -68,27 +73,27 @@ def saveimagesasnpy(dir='data/spectographs/', csvf='dekhabet_dataLabelsRanged.cs
     labels.pop(0)
     lens.pop(0)
     fnames.pop(0)
-    print('lfn:',len(fnames))
-    print('lbl:',len(labels))
-    print('lln:',len(lens))
+    print('lfn:', len(fnames))
+    print('lbl:', len(labels))
+    print('lln:', len(lens))
     length = len(fnames)
     imgarr = []
-    for index in range(0,length):
+    for index in range(0, length):
         image = Image.open(dir+fnames[index]+'.wav.jpg')
         nimage = image.resize((256, 128), Image.NEAREST)
         nimage = nimage.convert('RGB')
         img = np.array(nimage)
         imgarr.append(img)
-        if index%100==0:
-            print(index,'imgs added to array')
+        if index % 100 == 0:
+            print(index, 'imgs added to array')
     imgarr = np.array(imgarr)
     csvFile.close()
-    print(fnames[0],lens[0],labels[0])
-    print(fnames[1],lens[1],labels[1])
-    print(fnames[55],lens[55],labels[55])
-    np.save('kothaddekha_ImageArray_'+name+'.npy',imgarr)
-    np.save('kothaddekha_LabelArray_'+name+'.npy',labels)
-    np.save('kothaddekha_LenthArray_'+name+'.npy',lens)
+    print(fnames[0], lens[0], labels[0])
+    print(fnames[1], lens[1], labels[1])
+    print(fnames[55], lens[55], labels[55])
+    np.save('kothaddekha_ImageArray_'+name+'.npy', imgarr)
+    np.save('kothaddekha_LabelArray_'+name+'.npy', labels)
+    np.save('kothaddekha_LenthArray_'+name+'.npy', lens)
 
 # def saveimagesasnpy(dir='data/spectographs/', csvf='dekhabet_dataLabelsRanged.csv', name='2k2sec'):
 #     # filearray = []
@@ -155,9 +160,10 @@ def saveimagesasnpy_modular(path='data/spectographs/', name='MOD', length=200, s
         if count > 0:
             filearray.append(fn)
             count -= 1
-    if shuffle is True: random.shuffle(filearray)
+    if shuffle is True:
+        random.shuffle(filearray)
     imgarr = []
-    for index in range(0,length):
+    for index in range(0, length):
         image = Image.open(filearray[index])
         # if image.size[0] != image.size[1]:
         #     sqrsize = min(image.size)
@@ -169,12 +175,13 @@ def saveimagesasnpy_modular(path='data/spectographs/', name='MOD', length=200, s
         imgarr.append(img)
     imgarr = np.array(imgarr)
     # imgarr = np.moveaxis(imgarr,3,1)
-    np.save('kothaddekha_ImageArray_'+name+'_'+str(length)+'.npy',imgarr)
+    np.save('kothaddekha_ImageArray_'+name+'_'+str(length)+'.npy', imgarr)
 
 
-def loadnpyfiles(npyname,rootpath=''):
+def loadnpyfiles(npyname, rootpath=''):
     npzimg = np.load(rootpath+'kothaddekha_ImageArray_'+npyname+'.npy')
-    npzlbl = np.load(rootpath+'kothaddekha_LabelArray_'+npyname+'.npy',allow_pickle=True)
+    npzlbl = np.load(rootpath+'kothaddekha_LabelArray_' +
+                     npyname+'.npy', allow_pickle=True)
     npzlen = np.load(rootpath+'kothaddekha_LenthArray_'+npyname+'.npy')
     return npzimg, npzlbl, npzlen
 
@@ -185,7 +192,7 @@ def convertimagestotensor(dirname='data/spectographs/'):
     for fn in filenames:
         filearray.append(fn)
     length = len(filearray)
-    for index in range(0,length):
+    for index in range(0, length):
         image = Image.open(filearray[index])
         # if image.size[0] != image.size[1]:
         #     sqrsize = min(image.size)
@@ -205,7 +212,8 @@ class KD_DL(D.Dataset):
     def __init__(self, root, rpath=''):
         """ Intialize the dataset """
         self.root = root
-        self.imgarray, self.labels, self.lens = loadnpyfiles(root,rootpath=rpath)
+        self.imgarray, self.labels, self.lens = loadnpyfiles(
+            root, rootpath=rpath)
         self.len = len(self.imgarray)
 
     def __getitem__(self, index):
@@ -246,40 +254,40 @@ class KD_DL_Tensor(D.Dataset):
         return self.len
 
 
-class KD_DL_Raw(D.Dataset):
+# class KD_DL_Raw(D.Dataset):
 
-    def __init__(self, root):
-        """ Intialize the dataset """
-        self.filearray = []
-        self.labels = []
-        self.root = root
-        self.transform = transforms.ToTensor()
-        filenames = glob.glob(osp.join(self.root, '*.jpg'))
-        for fn in filenames:
-            self.filearray.append(fn)
-            self.labels.append(1)
-        with open(csvf, 'r') as csvFile:
-            reader = csv.reader(csvFile)
-            for row in reader:
-                self.labels.append(row[4])
-        csvFile.close()
-        self.len = len(self.filearray)
+#     def __init__(self, root):
+#         """ Intialize the dataset """
+#         self.filearray = []
+#         self.labels = []
+#         self.root = root
+#         self.transform = transforms.ToTensor()
+#         filenames = glob.glob(osp.join(self.root, '*.jpg'))
+#         for fn in filenames:
+#             self.filearray.append(fn)
+#             self.labels.append(1)
+#         with open(csvf, 'r') as csvFile:
+#             reader = csv.reader(csvFile)
+#             for row in reader:
+#                 self.labels.append(row[4])
+#         csvFile.close()
+#         self.len = len(self.filearray)
 
-    def __getitem__(self, index):
-        """ Get a sample from the dataset """
-        image = Image.open(self.filearray[index])
-        # if image.size[0] != image.size[1]:
-        #     sqrsize = min(image.size)
-        #     croptrans = transforms.CenterCrop((sqrsize,sqrsize))
-        #     image = croptrans(image)
-        nimage = image.resize((256, 128), Image.NEAREST)
-        nimage = nimage.convert('RGB')
-        label = self.labels[index]
-        return self.transform(nimage), label
+#     def __getitem__(self, index):
+#         """ Get a sample from the dataset """
+#         image = Image.open(self.filearray[index])
+#         # if image.size[0] != image.size[1]:
+#         #     sqrsize = min(image.size)
+#         #     croptrans = transforms.CenterCrop((sqrsize,sqrsize))
+#         #     image = croptrans(image)
+#         nimage = image.resize((256, 128), Image.NEAREST)
+#         nimage = nimage.convert('RGB')
+#         label = self.labels[index]
+#         return self.transform(nimage), label
 
-    def __len__(self):
-        """ Total number of samples in the dataset """
-        return self.len
+#     def __len__(self):
+#         """ Total number of samples in the dataset """
+#         return self.len
 
 
 # class KD_DL_RawTensor(D.Dataset):
@@ -325,37 +333,37 @@ class KD_DL_Raw(D.Dataset):
 
 ####################################################################
 
-def create_sepeate_static_numpy_valid(name,rp):
-    npim, nplb, npln = loadnpyfiles(name,rp)
-
+def create_sepeate_static_numpy_valid(name, rp):
+    npim, nplb, npln = loadnpyfiles(name, rp)
 
     npim_val, nplb_val, npln_val = npim[:50], nplb[:50], npln[:50]
     npim_trtst, nplb_trtst, npln_trtst = npim[50:], nplb[50:], npln[50:]
 
     print('saving!')
 
-    np.save('kothaddekha_ImageArray_'+name+'val.npy',npim_val)
-    np.save('kothaddekha_LabelArray_'+name+'val.npy',nplb_val)
-    np.save('kothaddekha_LenthArray_'+name+'val.npy',npln_val)
+    np.save('kothaddekha_ImageArray_'+name+'val.npy', npim_val)
+    np.save('kothaddekha_LabelArray_'+name+'val.npy', nplb_val)
+    np.save('kothaddekha_LenthArray_'+name+'val.npy', npln_val)
 
-    np.save('kothaddekha_ImageArray_'+name+'trtst.npy',npim_trtst)
-    np.save('kothaddekha_LabelArray_'+name+'trtst.npy',nplb_trtst)
-    np.save('kothaddekha_LenthArray_'+name+'trtst.npy',npln_trtst)
+    np.save('kothaddekha_ImageArray_'+name+'trtst.npy', npim_trtst)
+    np.save('kothaddekha_LabelArray_'+name+'trtst.npy', nplb_trtst)
+    np.save('kothaddekha_LenthArray_'+name+'trtst.npy', npln_trtst)
 
 
-def get_loaders(path,split_perc=0.7,batch_size=32,mode=0, rootpath=''):
+def get_loaders(path, split_perc=0.7, batch_size=32, mode=0, rootpath=''):
     # Simple dataset. Only save path to image and load it and transform to tensor when call getitem.
-    if mode==0:
-        dataset = KD_DL(path,rootpath)           # Numpy File
-    elif mode==1:
+    if mode == 0:
+        dataset = KD_DL(path, rootpath)           # Numpy File
+    elif mode == 1:
         dataset = KD_DL_Tensor(path)    # Numpy to Tensor
-    elif mode==2:
-        dataset = KD_DL_Raw(path)       # Raw Images
+    # elif mode==2:
+    #     dataset = KD_DL_Raw(path)       # Raw Images
     else:
-        raise Exception('Parameter MODE given was {}. But needs to be an integer (0-2)\n0: Numpy, 1: Npy-Tensor, 2: Raw'.format(mode))
+        raise Exception(
+            'Parameter MODE given was {}. But needs to be an integer (0-2)\n0: Numpy, 1: Npy-Tensor, 2: Raw'.format(mode))
 
     # total images in set
-    print(dataset.len,'images from the dataset')
+    print(dataset.len, 'images from the dataset')
 
     # divide dataset into training and validation subsets
     train_len = int(split_perc*dataset.len)
@@ -364,16 +372,18 @@ def get_loaders(path,split_perc=0.7,batch_size=32,mode=0, rootpath=''):
     print(len(train), len(valid))
 
     # Use the torch dataloader to iterate through the dataset
-    trainloader = D.DataLoader(train, batch_size=batch_size, shuffle=False, num_workers=0)
-    validloader = D.DataLoader(valid, batch_size=batch_size, shuffle=False, num_workers=0)
+    trainloader = D.DataLoader(
+        train, batch_size=batch_size, shuffle=False, num_workers=0)
+    validloader = D.DataLoader(
+        valid, batch_size=batch_size, shuffle=False, num_workers=0)
     return trainloader, validloader
+
 
 def main_func():
     path = '2k2sec_22class'
     rpath = 'data/numpy_arrays/22_class/'
     # Simple dataset. Only save path to image and load it and transform to tensor when call __getitem__.
     dlt, dlv = get_loaders(path, mode=0, rootpath=rpath)
-
 
     # get some images
     dataiter_tr = iter(dlt)
